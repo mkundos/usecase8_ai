fc60612d01616e20c5b160f9ee5a140bf2ffd067
Maksym Kundos <mkundos@atlassian.com>
2023-09-03 16:31:04 +0300

    UC#8 Setup the form

--- usecase8_ai/src/components/FormComponent.js
+++ usecase8_ai/src/components/FormComponent.js
@@ -25,19 +25,12 @@ const FormComponent = () => {
     const [successMessage, setSuccessMessage] = useState(null);
 
     const dispatch = useDispatch();
-    const storedFormData = useSelector((state) => state.form.formData);
 
     useEffect(() => {
         if (successMessage) {
             setSuccessMessage(null);
         }
     }, [formData]);
-
-    useEffect(() => {
-        if (storedFormData) {
-            setSuccessMessage('Form submitted successfully!');
-        }
-    }, [storedFormData]);
 
     useEffect(() => {
         const { firstName, lastName, email, message } = formData;
@@ -48,14 +41,17 @@ const FormComponent = () => {
         if (!email || !isEmail(email))
             newErrors.email = 'Valid email is required';
         if (!message || !isLength(message, { min: 10 }))
-            newErrors.message = 'Message should be 10+ characters long';
+            newErrors.message = 'Message should be at least 10 characters long';
 
         setErrors(newErrors);
         setIsValid(Object.keys(newErrors).length === 0);
     }, [formData]);
 
+    useEffect(() => {
+        setErrors({});
+    }, []);
+
     const handleChange = (e) => {
-        // setSuccessMessage(null);
         const { name, value } = e.target;
         setFormData({
             ...formData,
@@ -67,12 +63,19 @@ const FormComponent = () => {
         e.preventDefault();
         if (isValid) {
             dispatch(updateFormData(formData));
+            setSuccessMessage('Form submitted successfully!');
         }
+    };
+
+    const handleCancel = () => {
+        setFormData(initialFormState);
     };
 
     return (
         <div className={styles.formWrapper}>
-            {successMessage && <div>{successMessage}</div>}
+            {successMessage && (
+                <div className={styles.successMessage}>{successMessage}</div>
+            )}
             <form onSubmit={handleSubmit} className={styles.form}>
                 <FormInput>
                     <label htmlFor='firstName'>First Name</label>
@@ -128,6 +131,7 @@ const FormComponent = () => {
                         className={styles.input}
                         id='message'
                         name='message'
+                        rows='5'
                         value={formData.message}
                         onChange={handleChange}
                         required
@@ -140,13 +144,18 @@ const FormComponent = () => {
                 </FormInput>
 
                 <button
+                    className={styles.button}
                     type='button'
-                    onClick={() => console.log('Cancel button clicked')}
+                    onClick={handleCancel}
                 >
                     Cancel
                 </button>
 
-                <button type='submit' disabled={!isValid}>
+                <button
+                    className={styles.button}
+                    type='submit'
+                    disabled={!isValid}
+                >
                     Submit
                 </button>
             </form>
